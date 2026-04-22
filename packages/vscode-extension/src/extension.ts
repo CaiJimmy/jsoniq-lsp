aea1de2e23e48c7a0325e7f48839b634c21273bd
Jimmy Cai <jimmy@cai.im>
2026-04-22 14:39:46 +0200

    Import language server from vscode-extension as npm dependency instead of relative path
    Currently resolved using pnpm workspace protocol, it should still work when we release each package separately

--- packages/vscode-extension/src/extension.ts
+++ packages/vscode-extension/src/extension.ts
@@ -1,5 +1,3 @@
-import path from "node:path";
-
 import * as vscode from "vscode";
 import {
     LanguageClient,
@@ -11,7 +9,7 @@ import {
 let client: LanguageClient | undefined;
 
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
-    const serverModule = context.asAbsolutePath(path.join("..", "language-server", "dist", "server", "main.js"));
+    const serverModule = require.resolve("@jsoniq-lsp/language-server/server");
 
     const serverOptions: ServerOptions = {
         run: {
