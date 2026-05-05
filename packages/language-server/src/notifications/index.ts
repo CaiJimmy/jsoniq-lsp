c7753c585313585442a9f081b20359a00b613d99
Jimmy Cai <jimmy@cai.im>
2026-05-05 14:21:26 +0200

    Export notification types and constants for client

--- packages/language-server/src/notifications/index.ts
+++ packages/language-server/src/notifications/index.ts
@@ -1,6 +1,12 @@
 import { setWrapperResolutionOptions } from "server/wrapper/client.js";
 import { WRAPPER_DOWNLOAD_PROGRESS_NOTIFICATION } from "./types.js";
 import { Connection } from "vscode-languageserver/node.js";
+import { DownloadProgress } from "server/wrapper/executable/download.js";
+
+export { 
+    WRAPPER_DOWNLOAD_PROGRESS_NOTIFICATION,
+    type DownloadProgress,
+}
 
 export const initializeCustomNotifications = (connection: Connection): void => {
     setWrapperResolutionOptions({
